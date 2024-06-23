a3443863f98527e2e153a53638b8f68f0c3c1048
Enty2905 <nguyenthai05022000@gmail.com>
2024-06-23 21:38:41 +0700

    bwd

--- assets/js/show-row.js
+++ assets/js/show-row.js
@@ -1,7 +1,5 @@
 var slideImages = document.querySelectorAll('.slide-img');
 var mainImage = document.getElementById('main-image');
-var btnLeft = document.querySelector('.btn-left');
-var btnRight = document.querySelector('.btn-right');
 var slideContainer = document.querySelector('.slide');
 slideImages.forEach(img => {
     img.addEventListener('click', function () {
@@ -9,23 +7,35 @@ slideImages.forEach(img => {
     });
 });
 
-btnLeft.onclick = function () {
-    slideContainer.scrollTop -= 177;
-};
+function updateIcons() {
+    const btnLeft = document.querySelector('.btn-left i');
+    const btnRight = document.querySelector('.btn-right i');
 
-btnRight.onclick = function () {
-    slideContainer.scrollTop += 177;
-};
-function checkScreenSize() {
-    if (window.innerWidth <= 767) {
-        btnLeft.onclick = function () {
-            slideContainer.scrollTop -= 227;
-        };
-        
-        btnRight.onclick = function () {
-            slideContainer.scrollTop += 227;
-        };
+    if (window.innerWidth <= 567) {
+        btnLeft.classList.remove('fa-arrow-up');
+        btnLeft.classList.add('fa-arrow-left');
+        btnRight.classList.remove('fa-arrow-down');
+        btnRight.classList.add('fa-arrow-right');
+    } else {
+        btnLeft.classList.remove('fa-arrow-left');
+        btnLeft.classList.add('fa-arrow-up');
+        btnRight.classList.remove('fa-arrow-right');
+        btnRight.classList.add('fa-arrow-down');
+    }
+}
+window.addEventListener('load', updateIcons);
+window.addEventListener('resize', updateIcons);
+
+function scrollSlide(direction) {
+    const slide = document.querySelector('.slide');
+    const scrollAmount = 135;
+
+    if (direction === 'left') {
+        slide.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else if (direction === 'right') {
+        slide.scrollBy({ left: scrollAmount, behavior: 'smooth' });
     }
 }
 
-checkScreenSize();
+document.querySelector('.btn-left').addEventListener('click', () => scrollSlide('left'));
+document.querySelector('.btn-right').addEventListener('click', () => scrollSlide('right'));
